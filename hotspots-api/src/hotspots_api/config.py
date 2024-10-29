e71897f4cecbfc71644d95f7ab29100e80c8cb8d
ncortim <ncortim@proton.me>
2024-10-29 13:19:23 +0100

    update pydantic to pydantic settings for env vars management

--- hotspots-api/src/hotspots_api/config.py
+++ hotspots-api/src/hotspots_api/config.py
@@ -1,20 +1,19 @@
-"""Configuration File
-"""
-import os
+"""Configuration File"""
 
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    postgres_user: str  
-    postgres_pw: str  
-    postgres_host: str  
-    postgres_port: int 
-    postgres_db: str  
+    postgres_user: str
+    postgres_pw: str
+    postgres_host: str
+    postgres_port: int
+    postgres_db: str
     redis_host: str
     redis_port: int
 
     class Config:
         env_file = ".env"
+
 
 settings = Settings()
